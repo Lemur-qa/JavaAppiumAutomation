982a567072464da4d5fbf9154ad1068ce7f46d9e
ekaterinaperepechaeva <Ekateri.Perepechaeva@x5.ru>
2021-11-08 13:53:39 +0300

    Task 2

--- src/yoTest/MainClass.java
+++ src/yoTest/MainClass.java
@@ -6,4 +6,10 @@ public class MainClass
     {
         return 14;
     }
+    private int class_number = 20;
+
+    public int getClassNumber()
+    {
+        return class_number;
+    }
 }
--- test/java/MainClassTest.java
+++ test/java/MainClassTest.java
@@ -5,13 +5,21 @@ import yoTest.MainClass;
 public class MainClassTest {
     @Test
     public void testGetLocalNumber()
-        {
-            MainClass mainClass = new MainClass();
-            int result = mainClass.getLocalNumber();
-            Assert.assertEquals(result, 14);
+    {
+        MainClass mainClass = new MainClass();
+        int result = mainClass.getLocalNumber();
+        Assert.assertEquals(result, 14);
     }
     private  void assertFail()
     {
         Assert.fail("This message will be printed");
+    }
+    @Test
+    public void testGetClassNumber()
+    {
+        MainClass mainClass = new MainClass();
+        int result = mainClass.getClassNumber();
+        Assert.assertTrue("Error, number is too high", result < 45);
+        Assert.assertTrue("Error, number is too low",result > 45);
     }
 }
